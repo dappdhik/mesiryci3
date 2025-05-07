1a4f7883e7a3dad7e527d2092b20e8786c681e0d
penyimak527 <supriyantolumajang@gmail.com>
2025-05-07 12:08:30 +0700

    ngubah controller admin, view user dll, base url

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -1,9 +1,16 @@
 <?php
 class Barang extends CI_Controller{
+
+    public function __construct(){
+        parent::__construct( );
+        $this->load->model('Barang_model');
+    }
+
     public function index(){
         $data   = array(
             'halaman'   => 'pagedata/v_barang',
             'kasir' => 'pagedata/v_kasir',
+            'mhs'   =>  $this->Barang_model->barangsemua(),
         );
         $this->load->view('v_tampilan', $data);
     }
@@ -20,6 +27,17 @@ class Barang extends CI_Controller{
             'kasir' => 'pagedata/v_kasir',
         );
         $this->load->view('v_tampilan', $data);
+    }
+
+    public function inputb(){
+        $data = array(
+            // nama field dari database dimasukkan dari inputan
+            'nama_barang'   => $this->input->post('nama'),
+            'stok'          => $this->input->post('stok'),
+            'harga'         => $this->input->post('harga'),
+            
+        );
+        
     }
 }
 ?>
--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -2,17 +2,69 @@
 <html>
 <head>
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Login</h2>
+    
+    <?php if($this->session->flashdata('error')) : ?>
+        <div class="alert alert-danger"><?php echo $this->session->flashdata('error'); ?></div>
+    <?php endif; ?>
+    
+    <?php if(validation_errors()) : ?>
+        <div class="alert alert-danger"><?php echo validation_errors('<p style="color:red;">', '</p>'); ?></div>
+    <?php endif; ?>
+    
+<!-- Section: Design Block -->
+<section class="text-center text-lg-start">
+  <style>
+    .rounded-t-5 {
+      border-top-left-radius: 0.5rem;
+      border-top-right-radius: 0.5rem;
+    }
 
-    <?= $this->session->flashdata('error'); ?>
-    <?= validation_errors('<p style="color:red;">', '</p>'); ?>
+    @media (min-width: 992px) {
+      .rounded-tr-lg-0 {
+        border-top-right-radius: 0;
+      }
 
-    <form action="<?= base_url('auth/login') ?>" method="post">
-        <input type="text" name="username" placeholder="Username" value="<?= set_value('username'); ?>" required><br><br>
-        <input type="password" name="password" placeholder="Password" required><br><br>
-        <button type="submit">Login</button>
-    </form>
+      .rounded-bl-lg-5 {
+        border-bottom-left-radius: 0.5rem;
+      }
+    }
+  </style>
+  <div class="card mb-3 p-1 m-5">
+    <div class="row g-0 d-flex align-items-center">
+        <div class="col-lg-4 d-none d-lg-flex">
+            <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" alt="Trendy Pants and Shoes"
+            class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
+        </div>
+        
+        <div class="col-lg-8 p-3">
+            <h2>Login</h2>
+            <div class="card-body py-5 px-md-5">
+                <form action="<?php echo base_url('auth/login'); ?>" method="post">
+                    <!-- Username input -->
+                    <div class="form-outline mb-4">
+                        <input type="text" name="username" id="username" placeholder="Username" value="<?php echo set_value('username'); ?>" required class="form-control">
+                        <label class="form-label p-1" for="username">Username</label>
+                    </div>
+
+                    <!-- Password input -->
+                    <div class="form-outline mb-4">
+                        <input type="password" name="password" id="password" placeholder="Password" required class="form-control">
+                        <label class="form-label p-1" for="password">Password</label>
+                    </div>
+
+                    <!-- Submit button -->
+                    <button type="submit" class="btn btn-primary btn-block mb-4">Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  </div>
+</section>
+<!-- Section: Design Block -->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"></script>
 </body>
 </html>
